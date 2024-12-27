7e2dab26113e16464538124821d09d3799f33dfb
Jose Perez <84167692+Perezcortes@users.noreply.github.com>
2024-12-27 14:15:04 -0600

    Add files via upload

---
+++ general.js
@@ -0,0 +1,15 @@
+﻿// Cargar encabezado
+/*fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("header").innerHTML = data;
+    })
+    .catch((error) => console.error("Error al cargar el encabezado:", error));
+*/
+// Cargar pie de página
+fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("footer").innerHTML = data;
+    })
+    .catch((error) => console.error("Error al cargar el pie de página:", error));
